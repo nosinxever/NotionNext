5512cad1531baed436dd19ce6d79a498e58e4c99
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-09 22:00:46 +0800

    update header -- scroll  y  remain the same

--- themes/heo/components/Header.js
+++ themes/heo/components/Header.js
@@ -159,8 +159,10 @@ const Header = props => {
             <div
               className={`absolute transition-all duration-700 ${activeIndex === 1 ? 'opacity-100 mb-0' : '-mb-20 opacity-0 invisible'}`}>
               <h1 className='font-bold text-center text-light-400 dark:text-gray-400'>
-                {siteConfig('AUTHOR') || siteConfig('TITLE')}{' '}
-                {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
+                {/* {siteConfig('AUTHOR') || siteConfig('TITLE')}{' '}
+                {siteConfig('BIO') && <>|</>} {siteConfig('BIO')} */}
+                <MenuListTop {...props} />
+
               </h1>
             </div>
           </div>
