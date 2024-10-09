580da5f763164c29bd50448171ca9da4001f558c
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-09 17:57:06 +0800

    remove dashed line

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -68,7 +68,7 @@ export default function SideRight(props) {
             'bg-white dark:bg-[#1e1e1e] dark:text-white hover:border-indigo-600  dark:hover:border-yellow-600 duration-200'
           }>
           <TagGroups tags={sortedTags} currentTag={currentTag} />
-          <hr className='mx-1 flex border-dashed relative my-4' />
+          {/* <hr className='mx-1 flex border-dashed relative my-4' /> */}
           {/* <AnalyticsCard {...props} /> */}
         </Card>
       </div>
