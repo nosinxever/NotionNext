33e05b0ee1d005bebf1c3a0246dd31638ff5e7ee
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-09 16:33:29 +0800

    update heo theme config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,13 +4,10 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-10-09', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  HEO_NOTICE_BAR: [],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -18,19 +15,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: ' ',
+  HEO_HERO_TITLE_2: ' ',
+  HEO_HERO_TITLE_3: ' ',
+  HEO_HERO_TITLE_4: ' ',
+  HEO_HERO_TITLE_5: ' ',
+  HEO_HERO_TITLE_LINK: ' ',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Recommend',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'News', url: '/tag/Daily News' },
+  HEO_HERO_CATEGORY_2: { title: 'Rec', url: '/tag/Recommend' },
+  HEO_HERO_CATEGORY_3: { title: 'Thought', url: '/tag/Thought' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -39,22 +36,20 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    'Hello! I am',
+    '🔍 Sharing and Eager to Help',
+    '🤝 Specializing in Interaction and Design',
+    '🏃 Down-to-earth and Action-oriented',
+    '🏠  Smart Home Whiz',
+    '🤖️ Digital Technology Enthusiast',
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/nosinxever',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+ 
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -117,10 +112,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'Communication Channel',
+  HEO_SOCIAL_CARD_TITLE_2: 'Join Our Community Discussion and Share',
+  HEO_SOCIAL_CARD_TITLE_3: 'Click to Join the Community',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/+tbcDXSIU8aQ3MDYx',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
