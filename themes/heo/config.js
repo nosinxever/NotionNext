b2157b9f6f29ebc2440dacf2915d97027deffc53
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-09 16:46:36 +0800

    update heo config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,18 +30,13 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: 'Thought', url: '/tag/Thought' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: 'Rec',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    'Hello! I am',
-    '🔍 Sharing and Eager to Help',
-    '🤝 Specializing in Interaction and Design',
-    '🏃 Down-to-earth and Action-oriented',
-    '🏠  Smart Home Whiz',
-    '🤖️ Digital Technology Enthusiast',
+    'Welcome',
   ],
 
   // 个人资料底部按钮
