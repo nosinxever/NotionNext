162401ed9c2c8b4f69d9403bf5224f068d904a71
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-10 10:58:11 +0800

    update NEXT_PUBLIC_AUTHOR

--- blog.config.js
+++ blog.config.js
@@ -24,9 +24,9 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lishuuun', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'D00dle', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'I am practicing writing', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://lishuuun.vercel.app', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://D00dle.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blog, Post', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
