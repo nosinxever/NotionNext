9fe568b537d064c520403746ff2feb637c364759
nosinxever <85007597+nosinxever@users.noreply.github.com>
2024-10-10 20:11:56 +0800

    remove AUTHOR URL

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -25,7 +25,7 @@ export default function PostCopyright() {
   return (
     <section className='dark:text-gray-300 mt-6 mx-1 '>
       <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
-        <li>
+        {/* <li>
           <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
           <Link href={'/about'} className='hover:underline'>
             {siteConfig('AUTHOR')}
@@ -38,7 +38,7 @@ export default function PostCopyright() {
             href={path}>
             {path}
           </a>
-        </li>
+        </li> */}
         <li>
           <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
           {locale.COMMON.COPYRIGHT_NOTICE}
